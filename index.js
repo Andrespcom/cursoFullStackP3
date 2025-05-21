ba4290b5a31819d001d0c7e42f6d5230468de9aa
Andrespcom <andres.perez.comesana@gmail.com>
2025-05-21 14:29:03 +0200

    Ejercicios 3.1-3.6

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
 const express = require('express');
-const { resolve } = require('path');
-
 const app = express();
-const port = 3010;
+const PORT = 3001;
 
-app.use(express.static('static'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const persons = [
+  { id: 1, name: 'Arto Hellas', number: '040-123456' },
+  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/info', (req, res) => {
+  const total = persons.length;
+  const date = new Date();
+  res.send(`
+    <p>Phonebook has info for ${total} people</p>
+    <p>${date}</p>
+  `);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).send({ error: 'Person not found' });
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const index = persons.findIndex(p => p.id === id);
+
+  if (index !== -1) {
+    persons.splice(index, 1);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ error: 'Person not found' });
+  }
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number is missing' });
+  }
+
+  const nameExists = persons.find(p => p.name === body.name);
+  if (nameExists) {
+    return res.status(400).json({ error: 'name must be unique' });
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons.push(person);
+  res.status(201).json(person);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
